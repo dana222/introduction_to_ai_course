d8a48bf6f947823f726aecabda59d8b0145a96fd
dana222 <danaghassaan@gmail.com>
2025-12-12 16:44:55 +0300

    Update probabilistic_agent.py

--- course-project/RoboMind/agents/probabilistic_agent.py
+++ course-project/RoboMind/agents/probabilistic_agent.py
@@ -11,52 +11,47 @@ from ai_core.bayes_reasoning import bayes_update, update_belief_map
 
 
 class ProbabilisticAgent:
-    """
+     """
     An agent that uses Bayesian reasoning to handle uncertainty.
     """
-    #setting up the agent and passing the enviroment
     def __init__(self, environment: GridWorld): 
         """Initialize the probabilistic agent."""
-        self.env = environment # store the enviroment inside the agent
-        self.beliefs = {}  # store the beliefs inside agent, the belief is a dictionary storing what the agent believes about each cell
-        for row in range(self.env.height): # (self.env.height) is the no. of rows and we are looping around the grid 
-            for col in range(self.env.width): # no. of columns
-                self.beliefs[(row, col)] = 0.3 # assume that every cell has a 30% probability of obstacle
-        self.previousposition = None # the agent did not move so has no prev position
+        self.env = environment 
+        self.beliefs = {} 
+        for row in range(self.env.height): 
+            for col in range(self.env.width):
+                self.beliefs[(row, col)] = 0.3 
+        self.previousposition = None 
         
     def update_beliefs(self, sensor_reading, position):
         """Update beliefs using Bayes' rule."""
-        neighborcells = self.env.get_neighbors(position) # call func from enviroment.py
+        neighborcells = self.env.get_neighbors(position) 
         updatingbeliefs = {}
         for cell in neighborcells:
-            updatingbeliefs[cell] = self.beliefs[cell] # store the beliefs from self.beliefs into updatingbeliefs
+            updatingbeliefs[cell] = self.beliefs[cell] 
         
-        updatedbeliefs = update_belief_map(updatingbeliefs, sensor_reading, 0.9) # call func from bayes_reasoning where we are getting the posterior 
-        # update the agents belief map with the new probab
-        for cellposition, posterior in updatedbeliefs.items(): # store the posterior into the agent 
+        updatedbeliefs = update_belief_map(updatingbeliefs, sensor_reading, 0.9) 
+        for cellposition, posterior in updatedbeliefs.items(): 
             self.beliefs[cellposition] = posterior
 
-    # func where the agent moves
+
     def act(self):
         """Decide action based on probabilistic beliefs."""
-        currentposition = self.env.agent_pos # get current position
-        nextmoves = self.env.get_neighbors(currentposition) # call func from environment
-        # if there are no places to move, stay in the current pos
+        currentposition = self.env.agent_pos 
+        nextmoves = self.env.get_neighbors(currentposition) 
         if not nextmoves:
             return currentposition
-        # this removes the problem i encountered where the agent moves back and forth 
         moveoptions = [pos for pos in nextmoves if pos != self.previousposition]
         
         if len(moveoptions) == 0:
             moveoptions = nextmoves
-        safest_move = moveoptions[0] # assuming first move is safe
-        lowest_risk = self.beliefs.get(safest_move, 1.0) # the risk of the 1st move which is the least one to have an obstacle
-        # go through each move and and get its probab of an obstacle
+        safest_move = moveoptions[0] 
+        lowest_risk = self.beliefs.get(safest_move, 1.0) 
         for pos in moveoptions:
-            currentrisk = self.beliefs.get(pos, 1.0) # the risk of this move
-            if currentrisk < lowest_risk: # if the new risk is less than the 1st risk agent found before, move to it
+            currentrisk = self.beliefs.get(pos, 1.0)
+            if currentrisk < lowest_risk: 
                 lowest_risk = currentrisk
-                safest_move = pos # agent moves here
+                safest_move = pos 
         
         self.previousposition = currentposition
         return safest_move
